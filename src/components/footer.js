b6954b20d210f61a18ea06e814b66e9fc47da6a0
Daniel Jimenez <djimenez017@gmail.com>
2023-06-07 22:03:35 -0700

    completed footer layout and added meta tags

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+import navigationLinks from "../components/data/data";
+
+const FOOTER_STYLES = styled.footer`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
+  .column-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .column {
+    padding: 15px;
+  }
+
+  .column a {
+    text-decoration: none;
+    color: var(--black);
+    font-weight: bold;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+  }
+`;
+
+const doormatNavLinks = navigationLinks.map((link, index) => {
+  return (
+    <li key={index}>
+      <a href={link.href}>{link.name}</a>
+    </li>
+  );
+});
 
 export const footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <FOOTER_STYLES>
+      <div className="column-container">
+        <div className="column">Logo</div>
+        <div className="column">
+          Doormat Navigation <ul> {doormatNavLinks}</ul>
+        </div>
+        <div className="column">
+          Contact <ul> {doormatNavLinks}</ul>
+        </div>
+        <div className="column">
+          Social Media <ul> {doormatNavLinks}</ul>
+        </div>
+      </div>
+    </FOOTER_STYLES>
+  );
 };
 
 export default footer;
