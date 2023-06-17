d2672e4bc0dc168e973ef61b803ed746dd085113
Daniel Jimenez <djimenez017@gmail.com>
2023-06-17 01:35:58 -0700

    working on the cards

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,8 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-const Card = () => {
-  return <div>Card</div>;
+const CARD_STYLES = styled.div`
+  baackground: red;
+`;
+
+const Card = ({ name, price, description, src }) => {
+  return (
+    <CARD_STYLES>
+      <img src={src} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+      <p>{description}</p>
+    </CARD_STYLES>
+  );
 };
 
 export default Card;
--- src/components/ui/Specials.js
+++ src/components/ui/Specials.js
@@ -1,17 +1,28 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "./Button";
+import Card from "./Card";
+import menuItems from "../data/menu";
 
 const SPECIAL_STYLES = styled.div`
   display: flex;
+  flex-direction: column;
+  width: 95%;
+  max-width: 1000px;
+  margin: 0 auto;
 
   .specialContainer {
     display: flex;
     justify-content: space-between;
     width: 95%;
     max-width: 1000px;
-    margin: 0 auto;
+
     padding: 60px 0;
+  }
+
+  .cardContainer {
+    display: flex;
+    flex-direction: row;
   }
 
   h2 {
@@ -20,12 +31,24 @@ const SPECIAL_STYLES = styled.div`
 `;
 
 const Specials = () => {
+  const menuSpecial = menuItems.map((item, index) => {
+    return (
+      <Card
+        key={index}
+        name={item.name}
+        price={item.price}
+        description={item.description}
+        src={item.url}
+      />
+    );
+  });
   return (
     <SPECIAL_STYLES>
       <div className="specialContainer">
         <h2>Specials</h2>
         <Button>Online Menu</Button>
       </div>
+      <div className="cardContainer">{menuSpecial}</div>
     </SPECIAL_STYLES>
   );
 };
