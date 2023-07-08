2d3023c87c95101d224cfe7de235fbfd0d739e85
Daniel Jimenez <djimenez017@gmail.com>
2023-07-07 22:58:14 -0700

    adding changes

--- src/components/ui/TestimonialCard.js
+++ src/components/ui/TestimonialCard.js
@@ -1,13 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
+const TestimonialCardStyles = styled.div`
+  background: var(--sec-white);
+  margin: 20px;
+  img {
+    width: 70px;
+    border-radius: 50%;
+  }
+
+  .imageName {
+    display: flex;
+  }
+`;
 const TestimonialCard = ({ name, image_url, rating, text }) => {
+  console.log(image_url);
   return (
-    <div>
-      <h4>{name}</h4>
-      <img src={image_url} alt={name} />
+    <TestimonialCardStyles>
       <p>{rating}</p>
+      <div className="imageName">
+        <img src={image_url} alt={name} />
+        <h4>{name}</h4>
+      </div>
       <p>{text}</p>
-    </div>
+    </TestimonialCardStyles>
   );
 };
 
--- src/components/ui/Testimonials.js
+++ src/components/ui/Testimonials.js
@@ -3,11 +3,15 @@ import TestimonialCard from "./TestimonialCard";
 import styled from "styled-components";
 
 const TestimonialStyles = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 95%;
-  max-width: 1000px;
-  margin: 0 auto;
+  background: var(--primary-green);
+
+  .testimonialRow {
+    display: flex;
+    flex-direction: column;
+    width: 95%;
+    max-width: 1000px;
+    margin: 0 auto;
+  }
 
   .row {
     display: flex;
@@ -16,32 +20,28 @@ const TestimonialStyles = styled.div`
 
 const restaurant_reviews = [
   {
-    image_url: "https://randomuser.me/api/portraits/med/men/75.jpg",
+    image_url: "https://randomuser.me/api/portraits/thumb/men/7.jpg",
     rating: 4.5,
     name: "Michael Scott",
-    review_text:
-      "The food was absolutely delicious! The ambiance was cozy and the service was excellent. Highly recommend their pasta dishes.",
+    review_text: "The food was absolutely delicious!",
   },
   {
-    image_url: "https://randomuser.me/api/portraits/med/men/53.jpg",
+    image_url: "https://randomuser.me/api/portraits/thumb/men/5.jpg",
     rating: 3.8,
     name: "Jim Halpert",
-    review_text:
-      "Decent food but nothing extraordinary. The service was a bit slow, and the portions were smaller than expected. Prices were reasonable though.",
+    review_text: "Decent food but nothing extraordinary. ",
   },
   {
-    image_url: "https://randomuser.me/api/portraits/med/men/57.jpg",
+    image_url: "https://randomuser.me/api/portraits/thumb/men/15.jpg",
     rating: 5.0,
     name: "Dwight Schrutte",
-    review_text:
-      "One of the best dining experiences I've ever had! The flavors in each dish were incredible, and the presentation was top-notch. Will definitely be returning.",
+    review_text: "One of the best dining experiences I've ever had! ",
   },
   {
-    image_url: "https://randomuser.me/api/portraits/med/men/75.jpg",
+    image_url: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
     rating: 4.2,
     name: "Ryan the temp",
-    review_text:
-      "This place has a great atmosphere and friendly staff. The food was tasty, especially their signature dessert. Would recommend trying it out!",
+    review_text: "This place has a great atmosphere and friendly staff. ",
   },
 ];
 
@@ -49,7 +49,7 @@ const Testimonials = () => {
   const reviewCards = restaurant_reviews.map((review, index) => {
     return (
       <TestimonialCard
-        img={review.image_url}
+        image_url={review.image_url}
         rating={review.rating}
         name={review.name}
         text={review.review_text}
@@ -60,8 +60,10 @@ const Testimonials = () => {
 
   return (
     <TestimonialStyles>
-      <h2>Testimonials</h2>
-      <div className="row">{reviewCards}</div>
+      <div className="testimonialRow">
+        <h2>Testimonials</h2>
+        <div className="row">{reviewCards}</div>
+      </div>
     </TestimonialStyles>
   );
 };
